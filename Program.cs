f3108eadcad9155cdc62a43046fab789b1de9aed
lautaro gatti <lautarogatti98@gmail.com>
2024-04-25 02:10:36 -0300

    subida al repo

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Bienvenido al reconocedor de numeros");
+            Console.WriteLine("Ingrese el numero a reconocer");
+            numero = int.Parse(Console.ReadLine());
+            if(numero == 0){
+                Console.WriteLine("El numero " + numero + " es igual a 0");
+            } else if(numero > 0){
+                Console.WriteLine("El numero " + numero + " es positivo");
+            } else {
+                Console.WriteLine("El numero " + numero + " es negativo");
+            }
+        }
+    }
+}
